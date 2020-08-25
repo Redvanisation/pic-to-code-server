7958abf5f1d071bd82fd2313ef4535a9e15487b2
Redvan <redvanisation@gmail.com>
2020-08-25 17:16:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const vision = require('@google-cloud/vision');
+
+
+app.use(cors());
+
+  async function quickstart(req, res) {
+    // Imports the Google Cloud client library
+    // const vision = require('@google-cloud/vision');
+  
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient({
+      credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY
+
+      }
+    });
+  
+    // Performs label detection on the image file
+    const [result] = await client.documentTextDetection('./IMG_6807.jpg');
+    const labels = result.fullTextAnnotation;
+    console.log('Labels:');
+    // console.log(labels);
+    // console.log(labels.text);
+    // res.json(labels.text);
+    // console.log(process.env.PRIVATE_KEY)
+    // labels.pages.forEach(label => console.log(label.text));
+  }
+
+
+app.get('/', (req, res) => quickstart(req, res))
+
+app.listen(5000, () => console.log('server running on 5000'));
